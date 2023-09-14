6748c2f292737864e34cdca287dd3916d425cd80
zenitba <112856019+zenitba@users.noreply.github.com>
2023-09-14 15:08:00 +0200

    aanpassing voor de page test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,16 +12,16 @@
 	<div class="card-container">
 		<span class="pro">{data.label}</span>
 		<!-- svelte-ignore a11y-img-redundant-alt -->
-		<img class="round" src="{ data.zenit.url }" alt="profile photo" />
+		<img class="round" src="{ data.zenit.url }" width="100px" height="100px" alt="photo"/>
 		<h3>{data.title}</h3>
 		<p>{ data.bio[0].text }<br/></p>
-        <h6>{data.locatie}</h6>
+        <h4>{data.locatie}</h4>
 		<div class="buttons">
 			<button class="primary">{data.message}</button>
 			<button class="primary ghost"> {data.follow}</button>
 		</div>
 		<div class="skills">
-			<h6>{data.skill}</h6>
+			<h4>{data.skill}</h4>
 			<ul>
 				<li>{ data.skills[0].text}</li>
 			</ul>
